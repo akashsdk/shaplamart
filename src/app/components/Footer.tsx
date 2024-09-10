23f2e85ed928b15e0cd36b1c10218d0e181a7f7d
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-09-10 22:13:16 +0600

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,92 +1,19 @@
 "use client";
-import React from 'react'
+import React from 'react';
 
-function Footer() {
+const Footer: React.FC = () => {
   return (
-    <div className='bg-red-700'>
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      FooterFooter
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      FooterFooter
-      Footer
-      Footer
-      FooterFooter
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      FooterFooter
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      Footer
-      
-    </div>
-  )
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} TalkSpace. All rights reserved.</p>
+        <ul className="flex justify-center space-x-4 mt-4">
+          <li><a href="/about" className="hover:underline">About Us</a></li>
+          <li><a href="/contact" className="hover:underline">Contact</a></li>
+          <li><a href="/privacy" className="hover:underline">Privacy Policy</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
