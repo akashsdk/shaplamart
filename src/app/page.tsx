2f633168776827b6e1b06bb0b2b0b5e4fd1a5639
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-09-10 21:55:49 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
       Hello world! ok
     </h1>
-    <Link href="login">LohiN</Link>
+    <Link href="login">Lohin</Link>
     </div>
   );
 }
