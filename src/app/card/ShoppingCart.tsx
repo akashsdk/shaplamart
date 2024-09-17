05057e2736213753204b6bbfb5c2025deb7e630d
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-09-17 23:06:58 +0600

    Update ShoppingCart.tsx

--- src/app/card/ShoppingCart.tsx
+++ src/app/card/ShoppingCart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
 interface ShoppingCartProps {
@@ -31,34 +31,38 @@ const ShoppingCart: React.FC<ShoppingCartProps> = ({
   const discountedPrice = calculateDiscountedPrice(originalPrice, discount);
 
   return (
-    <div key={id} className="border rounded-md p-4 mb-4">
-      <div className="flex justify-between">
-        <div className="w-1/2">
-          {/* Main Product Image */}
-          <Image src={imageUrl} alt={name} className="rounded-lg w-full" />
+    // <div key={id} className="border rounded-md p-4 mb-4">
+    //   <div className="flex justify-between">
+    //     <div className="w-1/2">
+    //       {/* Main Product Image */}
+    //       <Image src={imageUrl} alt={name} className="rounded-lg w-full" />
           
-        </div>
+    //     </div>
 
-        {imageUrl2 && (
-          <div className="w-1/2 ml-4">
-            {/* Secondary Product Image */}
-            <Image src={imageUrl2} alt={`${name} secondary image`} className="rounded-lg w-full" />
-          </div>
-        )}
-      </div>
+    //     {imageUrl2 && (
+    //       <div className="w-1/2 ml-4">
+    //         {/* Secondary Product Image */}
+    //         <Image src={imageUrl2} alt={`${name} secondary image`} className="rounded-lg w-full" />
+    //       </div>
+    //     )}
+    //   </div>
 
-      <div className="mt-4">
-        <p className="font-bold text-lg">{name}</p>
-        <p className="text-gray-500">{categories}</p>
-        <p className="text-gray-600 mt-2">{details}</p>
+    //   <div className="mt-4">
+    //     <p className="font-bold text-lg">{name}</p>
+    //     <p className="text-gray-500">{categories}</p>
+    //     <p className="text-gray-600 mt-2">{details}</p>
 
-        <div className="flex items-center mt-2">
-          <p className="text-red-500 line-through mr-2">Tk {originalPrice}</p>
-          <p className="text-blue-600 font-bold">Tk {discountedPrice.toFixed(2)}</p>
-        </div>
+    //     <div className="flex items-center mt-2">
+    //       <p className="text-red-500 line-through mr-2">Tk {originalPrice}</p>
+    //       <p className="text-blue-600 font-bold">Tk {discountedPrice.toFixed(2)}</p>
+    //     </div>
 
-        <p className="text-green-600">{discount}% Off</p>
-      </div>
+    //     <p className="text-green-600">{discount}% Off</p>
+    //   </div>
+    // </div>
+
+    <div>
+        jjjj
     </div>
   );
 };
