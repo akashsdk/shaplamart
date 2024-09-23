20c3652771e31386aee910da6cb4f4a7c8dedecb
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-09-24 01:04:25 +0600

    ok

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,8 +3,22 @@ import React from "react";
 
 const Account: React.FC = () => {
   return (
-    <div className="flex items-center justify-center border-t">
-      <h1 className="text-3xl font-bold">Account uu</h1>
+    <div className="flex items-center justify-center flex-col border-t">
+      <div className="w-[95%] tablet:w-[90%] mt-10 tablet:mt-20 flex justify-between">
+        <p className="text-2xl tablet:text-4xl font-serif">My Account</p>
+        <button className="font-serif hover:text-blue-600">Logout</button>
+      </div>
+
+      <div className="w-[95%] tablet:w-[90%] mt-2 flex justify-between border-t">
+        <div className="tablet:w-[60%] tablet:mt-10">
+          <p className="text-xl tablet:text-2xl font-serif">Order History</p>
+          
+        </div>
+
+        <div className="tablet:w-[40%] tablet:mt-10">
+          <p className="text-xl tablet:text-2xl font-serif">Account Details</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/components/FloatButtonClient.tsx
+++ src/app/components/FloatButtonClient.tsx
@@ -5,11 +5,7 @@ import { FloatButton } from "antd";
 
 const FloatButtonClient = () => {
   return (
-    <>
-    <FloatButton.Group shape="square" type="primary" style={{ right: 14 }} className="bg-transparent">
-      <FloatButton.BackTop className="animatedFloatButton" visibilityHeight={0} />
-    </FloatButton.Group>
-    </>
+    <FloatButton.BackTop className="animatedFloatButton" visibilityHeight={0} />
   );
 };
 
