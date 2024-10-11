31b2a67842066414f8bce5c3b90db114b052cb69
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-10-11 21:28:20 +0600

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,11 +1,23 @@
 // shop.tsx
 "use client";
 import React from "react";
+import { UpOutlined } from "@ant-design/icons";
 
 const Shop: React.FC = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold">Shop</h1>
+    <div className="w-full flex justify-center items-center border-t">
+      <div className="w-[30%] mt-20">
+        <button className="w-[80%] ml-[10%] flex justify-between items-center hover:text-blue-600">
+          <p className="text-2xl">Categories</p>
+          <UpOutlined />
+        </button>
+        <div className="w-[80%] ml-[10%] mt-5 flex flex-col justify-start items-start">
+          <button>Accessories (22)</button>
+
+          <button>Baby (10)</button>
+        </div>
+      </div>
+      <div className="w-[70%] mt-20">Right</div>
     </div>
   );
 };
