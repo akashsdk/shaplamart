8ad5ad0305a3e206f95398c633b2a07ded12bb18
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-10-11 21:36:28 +0600

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,22 +1,47 @@
-// shop.tsx
 "use client";
-import React from "react";
-import { UpOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import { UpOutlined, DownOutlined } from "@ant-design/icons";
 
 const Shop: React.FC = () => {
+  const [page, setPage] = useState(1);
+
+  const pageTitles = [
+    "Contact Lenses",
+    "Comprehensive",
+    "Keratoconus",
+    "Phakic Lens",
+    "Retina Care",
+    "Glaucoma",
+  ];
+  
+  const [showCategories, setShowCategories] = useState(true); 
+
+  const toggleCategories = () => {
+    setShowCategories(prevState => !prevState); 
+  };
+
   return (
     <div className="w-full flex justify-center items-center border-t">
       <div className="w-[30%] mt-20">
-        <button className="w-[80%] ml-[10%] flex justify-between items-center hover:text-blue-600">
+        <button
+          onClick={toggleCategories} 
+          className="w-[80%] ml-[10%] flex justify-between items-center hover:text-blue-600"
+        >
           <p className="text-2xl">Categories</p>
-          <UpOutlined />
+          {showCategories ? <UpOutlined /> : <DownOutlined />} 
         </button>
-        <div className="w-[80%] ml-[10%] mt-5 flex flex-col justify-start items-start">
-          <button>Accessories (22)</button>
 
-          <button>Baby (10)</button>
+        
+        <div
+          className={`w-[80%] ml-[10%] mt-5 flex flex-col justify-start items-start overflow-hidden transition-all duration-500 ${
+            showCategories ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+          }`}
+        >
+          <button className="mb-2 opacity-70 hover:opacity-100">Accessories (22)</button>
+          <button className="mb-2 opacity-70 hover:opacity-100">Baby (10)</button>
         </div>
       </div>
+
       <div className="w-[70%] mt-20">Right</div>
     </div>
   );
