a85a31d28ae7572b1402552036994dff101ad077
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-10-11 21:53:20 +0600

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,48 +1,62 @@
 "use client";
 import React, { useState } from "react";
-import { UpOutlined, DownOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { UpOutlined, DownOutlined, SendOutlined } from "@ant-design/icons";
 
 const Shop: React.FC = () => {
   const [page, setPage] = useState(1);
 
   const pageTitles = [
-    "Contact Lenses",
-    "Comprehensive",
-    "Keratoconus",
-    "Phakic Lens",
-    "Retina Care",
-    "Glaucoma",
+    "Accessories",
+    "Baby",
+    "Women's Fashion",
+    "Men's Fashion",
+    "Footwear",
+    "New Arrivals",
   ];
-  
-  const [showCategories, setShowCategories] = useState(true); 
+
+  const [showCategories, setShowCategories] = useState(true);
 
   const toggleCategories = () => {
-    setShowCategories(prevState => !prevState); 
+    setShowCategories((prevState) => !prevState);
   };
 
   return (
     <div className="w-full flex justify-center items-center border-t">
       <div className="w-[30%] mt-20">
         <button
-          onClick={toggleCategories} 
+          onClick={toggleCategories}
           className="w-[80%] ml-[10%] flex justify-between items-center hover:text-blue-600"
         >
           <p className="text-2xl">Categories</p>
-          {showCategories ? <UpOutlined /> : <DownOutlined />} 
+          {showCategories ? <UpOutlined /> : <DownOutlined />}
         </button>
 
-        
         <div
           className={`w-[80%] ml-[10%] mt-5 flex flex-col justify-start items-start overflow-hidden transition-all duration-500 ${
-            showCategories ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+            showCategories ? "opacity-100" : "max-h-0 opacity-0"
           }`}
         >
-          <button className="mb-2 opacity-70 hover:opacity-100">Accessories (22)</button>
-          <button className="mb-2 opacity-70 hover:opacity-100">Baby (10)</button>
+          
+
+            {pageTitles.map((title, index) => (
+              <button
+                key={index}
+                className={`mb-2 opacity-70 hover:opacity-100 group ${
+                  page === index + 1 ? "text-blue-600" : ""
+                }`}
+
+                onClick={() => {
+                  setPage(index + 1);
+                }}
+              >
+               <p className="ml-3 font-semibold">{title}</p>
+              </button>
+            ))}
         </div>
       </div>
 
-      <div className="w-[70%] mt-20">Right</div>
+      <div className="w-[70%] mt-20">Rhuhhh</div>
     </div>
   );
 };
